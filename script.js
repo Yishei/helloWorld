ded8a0cb2e91636799e1d358d8c31bab9e97a785
k7WJaMGiZ4cBWvQ <yisheij1@gmail.com>
2023-11-02 23:45:02 -0400

    commit 4

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-  
-  function changeColor() {
-    var newColor = getRandomColor();
-    document.getElementById('greeting').style.color = newColor;
-  }
+  return color;
+}
+
+function changeColor() {
+  var newColor = getRandomColor();
+  document.getElementById("greeting").style.color = newColor;
+}
+
+function redirectToProtected() {
+  window.location.href = "/protected/index.html";
+}
